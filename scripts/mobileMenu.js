d1560a004399102061f6ef9ba4c8b7e1276278c3
zero_master <freirealexander0214@gmail.com>
2024-10-17 22:34:16 -0500

    saving():
    author: Zero

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -5,8 +5,17 @@ window.onload = function(){
 
     const activePage = window.location.pathname;
     const navLinks = document.querySelectorAll('.mobile-menu-item');
-    console.log(navLinks);
+    const headerMenuLinks = document.querySelectorAll('.header-menu > ul > li > a');
     navLinks.forEach(link => {
+        // Comparamos usando `startsWith` en caso de que haya subpáginas o parámetros
+        const linkHref = link.getAttribute('href');
+
+        // Comparación más estricta para evitar coincidencias incorrectas
+        if (activePage === linkHref || activePage.startsWith(linkHref + '/') || activePage.startsWith(linkHref + '/index.html')) {
+            link.classList.add('active');
+        }
+    });
+    headerMenuLinks.forEach(link => {
         // Comparamos usando `startsWith` en caso de que haya subpáginas o parámetros
         const linkHref = link.getAttribute('href');
 
