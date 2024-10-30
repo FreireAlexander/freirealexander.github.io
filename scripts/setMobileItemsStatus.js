a33e193da9d4f66781e5b50a7b7b58328d507517
zero_asus <zero@ASUS.>
2024-10-30 17:04:06 -0500

    fixbug():     - load components together for pages author: Zero |

--- scripts/setMobileItemsStatus.js
+++ scripts/setMobileItemsStatus.js
@@ -1,4 +1,4 @@
-function setMobileItemsStatus() {
+async function setMobileItemsStatus() {
     const activePage = window.location.pathname;
     const navLinks = document.querySelectorAll('.mobile-menu-item');
     const headerMenuLinks = document.querySelectorAll('.header-menu > ul > li > a');
