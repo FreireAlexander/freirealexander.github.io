2ccca81d914be884755872ab76cdb77e97ef3e90
zero_master <freirealexander0214@gmail.com>
2024-11-19 22:24:38 -0500

    feat():     - searchBar multilanguage author: Zero

--- components/topCards.js
+++ components/topCards.js
@@ -1,16 +1,15 @@
 import { icons } from "../scripts/utils.js";
 
-export function topCardBlog(item, language, counter) {
+export function topCardBlog(item, language) {
     const title = item.title[language] ? item.title[language] : item.title[""];
     const type = item.type[language] ? item.type[language] : item.type[""];
     
     const cardContentBlog = `
         <a class="card--top" href="${item.href}${language}">
-            <img 
-                ${counter===0 ? 'fetchpriority="high"' : `` }
+            <img
                 class="card-image" 
                 src="${item.href}coverPageSmall.webp" 
-                alt="${title}"
+                alt="cover page for ${title}"
             >
             <p>${title}</p>
         </a>
@@ -19,15 +18,14 @@ export function topCardBlog(item, language, counter) {
     return cardContentBlog;
 }
 
-export function topCardProject(item, language, counter) {
+export function topCardProject(item, language) {
     const title = item.title[language] ? item.title[language] : item.title[""];  
     const cardContentBlog = `    
             <a class="card--top" href="${item.href}${language}">
-                <img 
-                ${counter===0 ? 'fetchpriority="high"' : `` }
+                <img
                 class="card-image" 
                 src="${item.href}coverPageSmall.webp" 
-                alt="${title}"
+                alt="cover page for ${title}"
                 >
                 <p>${title}</p>
             </a>
